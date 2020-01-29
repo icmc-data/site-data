e8700cd8b9140989e8712c40e99b52cacbf40021
João Pedro Mattos <joao_pedro_mattos@hotmail.com>
2020-01-29 13:44:26 -0300

    Initial commit

---
+++ js/membros.js
@@ -0,0 +1,39 @@
+const membros = [
+  {
+    nome: "teste",
+    descricao: "loren ipsun",
+    img_uri: "teste.com.br/image"
+  },
+  {
+    nome: "teste",
+    descricao: "loren ipsun",
+    img_uri: "teste.com.br/image"
+  },
+  {
+    nome: "teste",
+    descricao: "loren ipsun",
+    img_uri: "teste.com.br/image"
+  },
+  {
+    nome: "teste",
+    descricao: "loren ipsun",
+    img_uri: "teste.com.br/image"
+  },
+  {
+    nome: "teste",
+    descricao: "loren ipsun",
+    img_uri: "teste.com.br/image"
+  },
+  {
+    nome: "teste",
+    descricao: "loren ipsun",
+    img_uri: "teste.com.br/image"
+  },
+  {
+    nome: "teste",
+    descricao: "loren ipsun",
+    img_uri: "teste.com.br/image"
+  }
+];
+
+
