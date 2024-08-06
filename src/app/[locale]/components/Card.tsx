db3d4486ac43f4040f051599c241a93a50dd817c
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-08-06 00:34:20 -0300

    feat: update cards

--- src/app/[locale]/components/Card.tsx
+++ src/app/[locale]/components/Card.tsx
@@ -11,11 +11,11 @@ const CardComponent: React.FC<CardComponentProps> = ({
   title = "Título Padrão",
   description = "Esta é uma descrição padrão do cartão, fornecida para o caso de não haver descrição especificada.",
 }) => (
-  <div className="bg-background-secondary rounded-lg shadow-md overflow-hidden w-80 transform transition-transform duration-300 hover:scale-105">
+  <div className="bg-background-secondary rounded-lg shadow-md overflow-hidden w-96 transform transition-transform duration-300 hover:scale-105">
     <img src={image} alt={title} className="w-full h-72 object-cover" />
     <div className="p-6">
-      <h2 className="text-primary font-brawler text-xl mb-2">{title}</h2>
-      <p className="text-text-secondary text-sm">{description}</p>
+      <h2 className="text-primary font-inter text-xl mb-2 font-bold  text-center">{title}</h2>
+      <p className="text-text-secondary text-description text-center">{description}</p>
     </div>
   </div>
 );
--- src/app/[locale]/pages/fronts/page.tsx
+++ src/app/[locale]/pages/fronts/page.tsx
@@ -5,12 +5,51 @@ import CardComponent from '../../components/Card';
 export default function Fronts() {
   const t = useTranslations();
 
+  const cardsData = [
+    {
+      image: "https://via.placeholder.com/250x300",
+      title: t('Study_Groups'),
+      description: t('Study_Groups_Description'),
+    },
+    {
+      image: "https://via.placeholder.com/250x300",
+      title: t('Projects'),
+      description: t('Projects_Description'),
+    },
+    {
+      image: "https://via.placeholder.com/250x300",
+      title: t('Events'),
+      description: t('Events_Description'),
+    },
+    {
+      image: "https://via.placeholder.com/250x300",
+      title: t('Marketing'),
+      description: t('Marketing_Description'),
+    },
+    {
+      image: "https://via.placeholder.com/250x300",
+      title: t('Secretariat'),
+      description: t('Secretariat_Description'),
+    },
+    {
+      image: "https://via.placeholder.com/250x300",
+      title: t('Teaching'),
+      description: t('Teaching_Description'),
+    },
+  ];
+
   return (
-    <div>
-      <CardComponent 
-        title={t('Study_Groups')}
-        description={t('Study_Groups_Description')}
-      />
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20">
+        {cardsData.map((card, index) => (
+          <CardComponent
+            key={index}
+            image={card.image}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
