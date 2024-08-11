6c547df09814907e060d75a6c68d67473c077634
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-08-11 02:11:26 -0300

    fix: everything went wrong, returning the commit 😭😭😭

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,32 +1,9 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
-const postsDirectory = path.join(process.cwd(), 'posts')
-
-export function getPosts() {
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map((fileName) => {
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-    const matterResult = matter(fileContents)
-
-    return {
-      id: fileName.replace(/\.md$/, ''),
-      ...matterResult.data,
-      content: matterResult.content,
-    }
-  })
-  return allPostsData
 }
--- src/app/[locale]/pages/competitions/page.tsx
+++ src/app/[locale]/pages/competitions/page.tsx
@@ -1,31 +1,17 @@
 import { useTranslations } from 'next-intl'
-import { getPosts } from '@/lib/utils'
 
 export default function Competitions() {
   const t = useTranslations('')
-  const posts = getPosts()
-
   return (
-    <div className="py-24 text-2xl">
+<div className="py-24 text-2xl">
       <section className="flex relative py-0">
         <div className="flex items-center">
           <div>
             <h1>{t("Header.Competitions")}</h1>
             <br />
             <br />
-            <div>
-              {posts.map((post) => (
-                <div key={post.id} className="mb-8">
-                  <h2 className="text-3xl font-bold">{post.title}</h2>
-                  <p className="text-sm text-gray-600">{post.date}</p>
-                  <p className="mt-2">{post.tags.join(', ')}</p>
-                  <div
-                    className="mt-4"
-                    dangerouslySetInnerHTML={{ __html: post.content }}
-                  />
-                </div>
-              ))}
-            </div>
+
+
           </div>
         </div>
       </section>
