fe0b81df3b786c05f7f188a9df14e0cb531a13f8
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-08-15 01:32:00 -0300

    feat: update search bar

--- src/app/[locale]/components/PostSearch.tsx
+++ src/app/[locale]/components/PostSearch.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PostList from './PostList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -21,6 +21,30 @@ export default function PostSearch({
 }: PostSearchProps) {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredPosts, setFilteredPosts] = useState(markdownFiles);
+  const [showSearchBar, setShowSearchBar] = useState(true);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentScrollTop > lastScrollTop) {
+        // Rolando para baixo
+        setShowSearchBar(false);
+      } else {
+        // Rolando para cima
+        setShowSearchBar(true);
+      }
+
+      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollTop]);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const term = event.target.value.toLowerCase();
@@ -42,18 +66,20 @@ export default function PostSearch({
 
   return (
     <div>
-      {!hideSearchBar && (
-        <div className="relative w-full mb-4">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <FontAwesomeIcon icon={faSearch} className="text-data-purple" />
-          </span>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            placeholder={" " + placeholderText} 
-            className="w-full p-2 pl-10 border rounded border-data-purple text-primary bg-background-secondary"
-          />
+      {showSearchBar && !hideSearchBar && (
+        <div className="sticky top-0 z-50 bg-background-secondary p-2 shadow-md rounded-t-lg"> {/* Adicionada a classe rounded-t-lg */}
+          <div className="relative w-full mb-4">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <FontAwesomeIcon icon={faSearch} className="text-data-purple" />
+            </span>
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              placeholder={" " + placeholderText} 
+              className="w-full p-2 pl-10 border rounded border-data-purple text-primary bg-background-secondary"
+            />
+          </div>
         </div>
       )}
       <PostList markdownFiles={filteredPosts} onPostClick={onPostClick} locale={locale} page="learn" />
