333d445eb1cf74f03c300b6874554596a665186a
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-09-29 16:24:10 -0300

    fix: deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,14 +8,24 @@ type CustomMiddleware = (req: NextRequest) => Promise<NextRequest | NextResponse
 const customMiddleware: CustomMiddleware = async req => {
   console.log('Custom middleware executed before next-intl');
 
-  // verifica se o cookie `preferredLocale` está presente
+  // Verifica se o cookie `preferredLocale` está presente
   const preferredLocale = req.cookies.get('preferredLocale')?.value;
 
   if (!preferredLocale) {
-    // seta o cookie `preferredLocale` como "br" se não existir
+    // Define o cookie `preferredLocale` como "br" se não existir
     const response = NextResponse.next();
     response.cookies.set('preferredLocale', 'br', { path: '/' });
     return response;
+  }
+
+  // Verifica se a URL não contém o locale e redireciona para o locale preferido
+  const pathname = req.nextUrl.pathname;
+
+  // Caso a rota não contenha o prefixo do locale
+  if (!locales.some(locale => pathname.startsWith(`/${locale}`))) {
+    const url = req.nextUrl.clone();
+    url.pathname = `/${preferredLocale}${pathname}`;
+    return NextResponse.redirect(url);
   }
 
   return req;
@@ -32,12 +42,12 @@ export default async function middleware(
 ): Promise<ReturnType<typeof intlMiddleware>> {
   const result = await customMiddleware(req);
 
-  // se `customMiddleware` retornar `NextResponse`, usar essa resposta
+  // Se `customMiddleware` retornar `NextResponse`, usar essa resposta
   if (result instanceof NextResponse) {
     return result;
   }
 
-  // caso contrário, continuar com o `intlMiddleware`
+  // Caso contrário, continuar com o `intlMiddleware`
   return intlMiddleware(result);
 }
 
